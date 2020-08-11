f9a28dfebf20fde257c3a1cd0ef3d86fa359d91b
Yotam Moss <yotam.mos@gmail.com>
2020-08-11 16:45:34 -0400

    global counter

--- main.go
+++ main.go
@@ -1,23 +1,31 @@
 package main
 
 import (
-	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"log"
 )
 
-type MyEvent struct {
-	Name string `json:"name"`
-	Age int 	`json:"age"`
+var invokeCount = 0
+var myObjects []*s3.Object
+
+func init() {
+	svc := s3.New(session.New())
+	input := &s3.ListObjectsV2Input{
+		Bucket: aws.String("yotammosbucket"),
+	}
+	result, _ := svc.ListObjectsV2(input)
+	myObjects = result.Contents
 }
 
-type MyResponse struct {
-	Message string `json:"Answer:"`
-}
-
-func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
-	return MyResponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
+func LambdaHandler() (int, error) {
+	invokeCount = invokeCount + 1
+	log.Print(myObjects)
+	return invokeCount, nil
 }
 
 func main() {
-	lambda.Start(HandleLambdaEvent)
+	lambda.Start(LambdaHandler)
 }
