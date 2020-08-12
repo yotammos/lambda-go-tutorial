865dc058ceb93eb54d662baf5e39fa2ec260c238
Yotam Moss <yotam.mos@gmail.com>
2020-08-11 20:07:32 -0400

    long running lambda

--- main.go
+++ main.go
@@ -1,31 +1,36 @@
 package main
 
 import (
+	"context"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-lambda-go/lambdacontext"
 	"log"
+	"time"
 )
 
-var invokeCount = 0
-var myObjects []*s3.Object
-
-func init() {
-	svc := s3.New(session.New())
-	input := &s3.ListObjectsV2Input{
-		Bucket: aws.String("yotammosbucket"),
-	}
-	result, _ := svc.ListObjectsV2(input)
-	myObjects = result.Contents
+func CognitoHandler(ctx context.Context) {
+	lc, _ := lambdacontext.FromContext(ctx)
+	log.Print(lc.Identity.CognitoIdentityPoolID)
 }
 
-func LambdaHandler() (int, error) {
-	invokeCount = invokeCount + 1
-	log.Print(myObjects)
-	return invokeCount, nil
+func LongRunningHandler(ctx context.Context) (string, error) {
+
+	deadline, _ := ctx.Deadline()
+	deadline = deadline.Add(-100 * time.Millisecond)
+	timeoutChannel := time.After(time.Until(deadline))
+
+	for {
+		select {
+			case <- timeoutChannel:
+				return "Finished before timing out.", nil
+
+		default:
+			log.Print("hello!")
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
 }
 
 func main() {
-	lambda.Start(LambdaHandler)
+	lambda.Start(LongRunningHandler)
 }
